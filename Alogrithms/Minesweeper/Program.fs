dcc7397b2c9ecd195b1b4df4cfd49eb10f158f50
Richie Jang <richiejang@naver.com>
2015-12-21 22:57:23 +0900

    Q2 : Starting

---
+++ Alogrithms/Minesweeper/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let getColAndRow (ins: string) =
+    let ls = ins.Split(' ')
+    (int ls.[0], int ls.[1])
+
+let solve (map: char[,]) =
+    printfn "%A" map
+    
+
+let mutable breaking = false
+while (not breaking) do
+    let col,row = getColAndRow (Console.ReadLine())
+    if col = 0 && row = 0 then breaking <- true
+    else
+        let map = Array2D.zeroCreate row col
+        for i = 0 to row-1 do
+            let input = Console.ReadLine()
+            input |> Seq.iteri
+                (fun index k -> map.[i,index] <- k)
+        solve map
+
+Console.ReadLine() |> ignore
